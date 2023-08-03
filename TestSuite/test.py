f93afe6aa09503a9c68a645ea042a0327f98302a
Yep Devenv <Devenv_1390@outlook.com>
2023-08-03 14:06:26 +0800

    引入BeautifulReport

--- TestSuite/test.py
+++ TestSuite/test.py
@@ -1,9 +1,13 @@
 import unittest
 
+from BeautifulReport import BeautifulReport
+
 from TestCase import test_app_unit
 
 suite = unittest.TestSuite()
 TestCases = unittest.TestLoader().loadTestsFromModule(test_app_unit)
 suite.addTest(TestCases)
-runner = unittest.TextTestRunner()
-runner.run(suite)
+# runner = unittest.TextTestRunner()
+# runner.run(suite)
+runner = BeautifulReport(suite)
+runner.report("测试报告", "report.html")
